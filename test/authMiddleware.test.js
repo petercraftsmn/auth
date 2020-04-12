0052cdafb5b1e7f25cb2e067e1c787ff599d19ec
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2020-04-12 14:33:06 -0400

    headerToJwtAuthorization implemented

--- test/authMiddleware.test.js
+++ test/authMiddleware.test.js
@@ -27,6 +27,7 @@ describe( 'Auth middleware test', function () {
 
             jwtMiddleware.bodyJwtToUser( req, res, nextFunc )
                 .then( () => {
+                    // console.log( req );
                     // console.log( req.parsed_token );
                     assert.strictEqual( req.parsed_token.header.alg, jwtHeader.alg,
                         'Parsed jwt header are not same as given header' );
@@ -72,6 +73,93 @@ describe( 'Auth middleware test', function () {
             let res = {};
 
             jwtMiddleware.bodyJwtToUser( req, res, nextFunc )
+                .then( () => {
+                    // console.log( req );
+                    assert.strictEqual( req.user._id, null,
+                        'Parsed user._id is not null' );
+                    done();
+                } )
+                .catch( err => done( err ) );
+        } );
+
+        it( 'Correct jwt token incoming in req.header.Authorization', function ( done ) {
+
+            let jwtHeader = { alg: "sha256" };
+            let jwtBody = { name: "peter singh", id: "128837730383" };
+            let token = jwtMiddleware.createJWT( jwtHeader, jwtBody );
+
+            let req = {
+                'Authorization': token,
+                get: function ( headerName ) {
+                    if ( headerName === 'Authorization' ) return this[ headerName ];
+                    else {
+                        return false;
+                    }
+                }
+            };
+
+            let res = {};
+
+            jwtMiddleware.headerJwtToUser( req, res, nextFunc )
+                .then( () => {
+                    // console.log( req );
+                    // console.log( req.parsed_token );
+                    assert.strictEqual( req.parsed_token.header.alg, jwtHeader.alg,
+                        'Parsed jwt header are not same as given header' );
+                    assert.strictEqual( req.parsed_token.payload._id, jwtBody._id,
+                        'Parsed jwt payload._id is not correct' );
+                    done();
+                } )
+                .catch( err => done( err ) );
+        } );
+
+        it( 'Bad jwt token incoming in req.header.Authorization', function ( done ) {
+
+            let bad_token = 'chARxIg-3S6nr7molw8VQokZD7r-hzC1WXIR7IFNIiKIe47bscH9SCmJk3SAE_cDyDNVPd848QDj7i54' +
+                '5BlMkSvi6Fqsnw-J1iP7XNb53npAiKYAboCSH9ZECYa5xyrJP-87ZQYbzY2zkNjzKDdIrc4m5bhi0hutchR_EHdg' +
+                'l4Z9LVKtDAZ87NAAZ8x4i4SDEYENKa7l6UwH-md6kbbKXnfSHG5Z-y8ksqlWnY2II-NXRvbHjZxRz20mKjrRFYGI' +
+                'aWKXASvAghPI_xNBq9SddPk16TX9n-QEk5m6F7ozoslJhmt0alXCuvW0L-EdQMfveSj9koY1I6Mr9q1lxEvbflUT' +
+                'DrNw2v-MS90k=';
+
+            let req = {
+                'Authorization': bad_token,
+                get: function ( headerName ) {
+                    if ( headerName === 'Authorization' ) return this[ headerName ];
+                    else {
+                        return false;
+                    }
+                }
+            };
+
+            let res = {};
+
+            jwtMiddleware.headerJwtToUser( req, res, nextFunc )
+                .then( () => {
+                    // console.log( req );
+                    assert.strictEqual( req.parsed_token.header, false,
+                        'Parsed jwt header is not false for wrong token' );
+                    assert.strictEqual( req.parsed_token.payload, 'token error',
+                        'Token error is not present' );
+                    done();
+                } )
+                .catch( err => done( err ) );
+        } );
+
+        it( 'Undefined req.header.Authorization', function ( done ) {
+
+            let req = {
+                'Authorization': null,
+                get: function ( headerName ) {
+                    if ( headerName === 'Authorization' ) return this[ headerName ];
+                    else {
+                        return false;
+                    }
+                }
+            };
+
+            let res = {};
+
+            jwtMiddleware.headerJwtToUser( req, res, nextFunc )
                 .then( () => {
                     // console.log( req );
                     assert.strictEqual( req.user._id, null,
