a330d6d285ac88d4da1ece73e1e471ae53df1bf5
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2020-04-12 14:10:34 -0400

    parseWebTokenFromAuthorizationHeader implemented

--- test/authUtil.test.js
+++ test/authUtil.test.js
@@ -5,11 +5,30 @@
  ******************************************************************************/
 
 const assert = require( 'assert' ).strict;
-const AuthUtil = require( '../lib/AuthUtil' );
+const authUtil = require( '../lib/AuthUtil' );
 
 describe( 'AuthUtil test', () => {
+
+    let req = {
+        'Authorization': 'Some header',
+        get: function ( headerName ) {
+            if ( headerName === 'Authorization' ) return this[ headerName ];
+            else {
+                return false;
+            }
+        }
+    };
+
     describe( 'testing auth util methods success and failure', () => {
-        it('')
+        it( 'parses web token from Authorization Header', ( done ) => {
+            authUtil.parseWebTokenFromAuthorizationHeader( req )
+                .then( () => {
+                    assert.deepStrictEqual( req.user.webToken, req.Authorization,
+                        'Web token in not present.')
+                    done();
+                } )
+                .catch( done )
+        } );
 
     } );
 } );
